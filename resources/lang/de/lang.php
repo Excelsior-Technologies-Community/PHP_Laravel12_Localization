bb9ced070993ec99e18b719d016a5f615b90a8ac
YashPatel4268 <142138437+YashPatel4268@users.noreply.github.com>
2026-09-04 17:18:02 +0530

    feat: add dynamic language switching, persistent locale, localized formatting, and browser language detection

--- resources/lang/de/lang.php
+++ resources/lang/de/lang.php
@@ -1,4 +1,47 @@
 <?php
+
 return [
-    'msg' => 'Laravel Internationalisierung Beispiel.'
+
+    'msg' => 'Laravel Internationalisierung Beispiel.',
+
+    'page_title' => 'Laravel Lokalisierung',
+
+    'page_description' => 'Demonstration einer mehrsprachigen Anwendung',
+
+    'current_language' => 'Aktuelle Sprache',
+
+    'select_language' => 'Sprache auswählen',
+
+    'english' => 'Englisch',
+
+    'french' => 'Französisch',
+
+    'german' => 'Deutsch',
+
+    'translated_message_label' => 'Übersetzte Nachricht',
+
+    'locale' => 'Anwendungssprache',
+
+    'fallback_locale' => 'Fallback-Sprache',
+
+    'session_language' => 'Sitzungssprache',
+
+    'locale_formatting' => 'Sprachabhängige Formatierung',
+
+    'formatted_date' => 'Formatierter Datumswert',
+
+    'formatted_number' => 'Formatierte Zahl',
+
+    'browser_detection' => 'Automatische Browser-Spracherkennung',
+
+    'browser_detection_description' => 'Die Sprache Ihres Browsers wird beim ersten Besuch automatisch erkannt. Ihre manuell ausgewählte Sprache hat Vorrang.',
+
+    'browser_language' => 'Browsersprache',
+
+    'language_changed' => 'Die Sprache wurde erfolgreich geändert.',
+
+    'invalid_language' => 'Die ausgewählte Sprache wird nicht unterstützt.',
+
+    'footer' => 'Laravel-Lokalisierungsdemo',
+
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\LocalizationController;
 
-Route::get('localization/{locale}', [LocalizationController::class, 'index']);
+Route::get('/', [LocalizationController::class, 'index'])
+    ->name('localization.index');
+
+Route::get('/localization', [LocalizationController::class, 'index'])
+    ->name('localization.index');
+
+Route::get('/localization/{locale}', [LocalizationController::class, 'changeLanguage'])
+    ->name('localization.change');
